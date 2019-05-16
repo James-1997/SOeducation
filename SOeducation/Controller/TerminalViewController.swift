6e7019513b7fff1485c8d83cdc05514b4fd06395
Robson James <james.robson.junior@gmail.com>
2019-05-15 22:25:02 -0400

    Terminal View e Interação de digitar comando completa

--- SOeducation/Controller/TerminalViewController.swift
+++ SOeducation/Controller/TerminalViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class TerminalViewController: UIViewController {
     
+    var clickBool: Bool = false
+    var command: String = ""
+    var logCommand: [String] = []
+    
+    @IBOutlet weak var TextTerminal: UILabel!
+    
+    @IBOutlet weak var textFieldCommand: UITextField!
     
     @IBOutlet weak var BackGroundTerminal: UIView!
     
@@ -20,6 +27,22 @@ class TerminalViewController: UIViewController {
         BackGroundTerminal.backgroundColor = backGroundTerminalColor
         
     }
+    
+    
+    @IBAction func commandSend(_ sender: Any) {
+        //Comando TextField
+       // TextTerminal.textColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        let command = textFieldCommand.text!
+        logCommand.append(command)
+        TextTerminal.text = ""
+        for cmd in logCommand {
+            TextTerminal.text! += ("\n \(cmd)")
+            print("\(cmd)")
+        }
+        
+    }
+    
+    
     
     override var prefersStatusBarHidden: Bool{
         return true
